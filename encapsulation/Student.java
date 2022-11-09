9ba1ed3f3d5a15573f3b7dbb9c6ab773857ca24b
sipicodes <94324191+PrincessSipi@users.noreply.github.com>
2022-11-09 12:01:13 +0000

    Add files via upload

---
+++ encapsulation/Student.java
@@ -0,0 +1,12 @@
+//A Java class which has only getter methods.  
+public class Student
+{  
+	//private data member  
+	private String college="AKG";  
+
+	//getter method for college  
+	public String getCollege()
+	{  
+		return college;  
+	}  
+} 
---
+++ encapsulation/Student1.java
@@ -0,0 +1,12 @@
+//A Java class which has only setter methods.  
+public class Student1
+{  
+	//private data member  
+	private String college;
+  
+	//getter method for college  
+	public void setCollege(String college)
+	{  
+		this.college=college;  
+	}  
+} 
